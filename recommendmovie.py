0449d4ca2beb38d113fd986f6bcc108fa0debf83
Sam Wlody <SWlody@gmail.com>
2018-04-06 21:41:45 -0400

    Implemented basic collaborative filtering that seems to kind of work

--- recommendmovie.py
+++ recommendmovie.py
@@ -1,50 +1,109 @@
-from __future__ import print_function
+from __future__ import print_function, division
 from argparse import ArgumentParser
-from collections import namedtuple
+from math import sqrt
 import csv
 
 
 def init_parser():
-    # TODO Should allow user to specify TVDB / MovieLens movie ids instead of IMDb, if desired
-    parser = ArgumentParser(description="Recommend a similar movie.")
+    parser = ArgumentParser(description="Given a User ID from the MovieLens database, "
+                                        "predict the user's scores for specified movies.")
     parser.add_argument('user_id', metavar='user-id',
-                        help='The User ID from ratings.csv to predict the ratings of.')
-    parser.add_argument('imdb_ids', metavar='movies', nargs='+',
-                        help='A list of movies (IMDB IDs) to predict the ratings of.')
+                        help='The User ID from ratings.csv to predict ratings for.')
+    parser.add_argument('ids', metavar='movies', nargs='+',
+                        help='A list of movies (IMDb IDs) to predict the ratings of.')
     parser.add_argument('-f', '--full', action='store_true',
                         help="Use the full dataset rather than the small dataset.")
+    parser.add_argument('-m', '--movielens', action='store_true',
+                        help='Specify that the IDs are MovieLens IDs rather than IMDb IDs.')
+    parser.add_argument('-t', '--tmdb', action='store_true',
+                        help='Specify that IDs are TMDb IDs rather than IMDb IDs.')
+    parser.add_argument('-c', '--cosine', action='store_true',
+                        help="User cosine similiarity instead of Pearson correlation to "
+                        "calculate distances for collaborative filtering.")
     return parser
 
 
 def main():
     parser = init_parser()
     args = parser.parse_args()
-    movie_ids = get_movie_ids_from_imdb_ids(args.imdb_ids, args.full)
-    for movie, rating in get_predicted_ratings(args.user_id, movie_ids, args.full):
-        print("Movie:", movie, "Predicted rating:", rating)
+    if args.movielens:
+        movie_ids = args.ids
+    elif args.tmdb:
+        movie_ids = get_movie_ids_from_webdb_ids(args.ids, args.full, tmdb=True)
+    else:
+        movie_ids = get_movie_ids_from_webdb_ids(args.ids, args.full)
+    for movie, rating in get_predicted_ratings(args.user_id, movie_ids, args.full, args.cosine):
+        print("Movie:", movie, "Predicted rating:", round(rating) / 2, "stars")
 
 
-def get_predicted_ratings(user_id, movie_ids, full):
-    movie_names = get_movie_names(movie_ids, full)
-    our_users_ratings, other_user_ratings = get_relevant_user_ratings(user_id, movie_ids, full)
+def get_predicted_ratings(user_id, movie_ids, full, cosine):
+    """For a given User ID, predict ratings for each movie in the list of MovieLens IDs using collaborative filtering.
+    "full" specifies that that full database should be used, and "cosine" specifies that 
+    the cosine similarity distance function should be used instead of Pearson correlation"""
+    our_user_ratings, other_users_ratings = get_relevant_user_ratings(user_id, movie_ids, full)
+    movies = get_movies_from_ids(movie_ids, full)
     for movie_id in movie_ids:
-        yield movie_names[movie_id], get_rating(movie_id)
+        yield movies[movie_id], get_rating(movie_id, our_user_ratings, other_users_ratings, cosine)
 
 
-def get_movie_ids_from_imdb_ids(imdb_ids, full):
+def pearson_correlation(our_user_ratings, other_user_ratings, our_avg, other_avg):
+    """Computer the Pearson correlation between two users, given a list of
+    their ratings and their precomputed average rating."""
+    numer = 0
+    for movie_id, our_rating in our_user_ratings.items():
+        if movie_id in other_user_ratings:
+            numer += (our_rating - our_avg) * (other_user_ratings[movie_id] - other_avg)
+    denom = sum((rating - our_avg) ** 2 for movie_id, rating in our_user_ratings.items())
+    denom *= sum((rating - other_avg) ** 2 for movie_id, rating in other_user_ratings.items())
+    return numer / sqrt(denom)
+
+
+def cosine_similarity(our_user_ratings, other_user_ratings):
+    """Computer the cosine similarity between two users, given a list of
+    their ratings and their precomputed average rating."""
+    numer = 0
+    for movie_id, our_rating in our_user_ratings.items():
+        if movie_id in other_user_ratings:
+            numer += our_rating * other_user_ratings[movie_id]
+    denom = sum(rating for movie_id, rating in our_user_ratings.items()) ** 2
+    denom *= sum(rating for movie_id, rating in other_user_ratings.items()) ** 2
+    return numer / sqrt(denom)
+
+
+def get_rating(movie_id, our_user_ratings, other_user_ratings, cosine):
+    """Given a Movie ID and a list of our user's ratings, """
+    our_avg = sum(rating for movie_id, rating in our_user_ratings.items()) / len(our_user_ratings)
+    numer = 0
+    denom = 0
+    for user, ratings in other_user_ratings.items():
+        other_avg = sum(rating for mid, rating in ratings.items()) / len(ratings)
+        diff = (ratings[movie_id] - other_avg)
+        if cosine:
+            weight = cosine_similarity(our_user_ratings, ratings)
+        else:
+            weight = pearson_correlation(our_user_ratings, ratings, our_avg, other_avg)
+        numer += diff * weight
+        denom += abs(weight)
+    return our_avg + (numer / denom)
+
+
+def get_movie_ids_from_webdb_ids(ids, full, tmdb=False):
     """Given a list of IMDb IDs, return a list of Movie IDs corresponding to the same movie in the database"""
     filename = 'ml-latest/links.csv' if full else 'ml-latest-small/links.csv'
     with open(filename) as file:
         csvreader = csv.reader(file)
-        return [movie_id for movie_id, imdb_id, tvdb_id in csvreader if imdb_id in imdb_ids]
+        if tmdb:
+            return [movie_id for movie_id, imdb_id, tmdb_id in csvreader if tmdb_id in ids]
+        else:
+            return [movie_id for movie_id, imdb_id, tmdb_id in csvreader if imdb_id in ids]
 
 
-def get_movie_names(movie_ids, full):
+def get_movies_from_ids(movie_ids, full):
     """Return a mapping from Movie IDs to titles"""
     filename = 'ml-latest/movies.csv' if full else 'ml-latest-small/movies.csv'
     with open(filename) as file:
         csvreader = csv.reader(file)
-        return {movie_id: title for movie_id, title, genres in csvreader if movie_id in movie_ids}
+        return {movie_id: (title, genres.split('|')) for movie_id, title, genres in csvreader if movie_id in movie_ids}
 
 
 def get_relevant_user_ratings(user_id, movie_ids, full):
@@ -53,27 +112,28 @@ def get_relevant_user_ratings(user_id, movie_ids, full):
     filename = 'ml-latest/ratings.csv' if full else 'ml-latest-small/ratings.csv'
     with open(filename) as file:
         csvreader = csv.reader(file)
+        # skip header
+        next(csvreader)
         # Mapping from user ids to a list of movie ratings
-        other_user_ratings = {}
+        other_users_ratings = {}
         # Running list of the current user id ratings
-        user_ratings = []
-        Rating = namedtuple('Rating', ['movie_id', 'rating'])
-        curr_uid = 0
+        user_ratings = {}
+        current_uid = 0
         insert = False
-        for uid, mid, rating, timestamp in csvreader:
-            if curr_uid != uid:
-                if user_id == curr_uid:
+        for uid, movie_id, rating, timestamp in csvreader:
+            if current_uid != uid:
+                if user_id == current_uid:
+                    # FIXME This breaks for the last user in the database!!!
                     our_user_ratings = user_ratings
                 elif insert:
-                    other_user_ratings[curr_uid] = user_ratings
-                curr_uid = uid
-                user_ratings = []
+                    other_users_ratings[current_uid] = user_ratings
+                current_uid = uid
+                user_ratings = {}
                 insert = False
-            if mid in movie_ids:
+            if movie_id in movie_ids:
                 insert = True
-            user_ratings.append(Rating(mid, rating))
-    other_user_ratings = []
-    return our_user_ratings, other_user_ratings
+            user_ratings[movie_id] = int(float(rating) * 2)
+    return our_user_ratings, other_users_ratings
 
 
 if __name__ == '__main__':
